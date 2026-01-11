bae0070a97424c31eb7bba4a60b3a26d26a994f4
Inaba1995 <149956151+Inaba1995@users.noreply.github.com>
2026-01-11 22:56:38 +0300

    1

---
+++ django1/main/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from . import views
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('data', views.data, name='data'),
+    path('test', views.test, name='test'),
+
+
+
+]
