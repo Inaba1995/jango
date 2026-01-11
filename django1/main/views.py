bdfe6f304e10c36d9babee09456ede02ca730f16
Inaba1995 <149956151+Inaba1995@users.noreply.github.com>
2026-01-11 22:59:30 +0300

    2

--- django1/main/views.py
+++ django1/main/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1> Это мой первый проект на Django </h1>")
+    return HttpResponse("<h1> Мистер Django передает привет (00)</h1>")
 
 def data(request):
     return HttpResponse("<h1> Я правильно понял домашнее задание? @_@ </h1>")
